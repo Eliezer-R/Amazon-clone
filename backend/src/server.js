5634190ade49e8739bb90705dd2ec0fb6cb56e12
Eliezer .R <ronaldalmanzarvasques@gmail.com>
2025-09-22 12:12:08 -0400

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -12,10 +12,23 @@ dotenv.config()
 const app = express()
 const PORT = process.env.PORT || 5000
 
+const allowedOrigins = [
+  'http://localhost:3000',
+  process.env.API_URL
+]
+
 // Middlewares
 app.use(
   cors({
-    origin: 'http://localhost:3000',
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true)
+      } else {
+        callback(new Error('No permitido por CORS'))
+      }
+    },
+     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
     credentials: true
   })
 )
