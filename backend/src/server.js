8b90f3fcc891e419c658e429e6c03d0f378b4b49
Eliezer .R <ronaldalmanzarvasques@gmail.com>
2025-09-28 03:19:45 -0400

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -12,7 +12,7 @@ dotenv.config()
 const app = express()
 const PORT = process.env.PORT || 5000
 const allowedOrigins = [
-  "https://tu-frontend.vercel.app",
+  "https://localhost/3000",
    process.env.API_URL
 ];
 
