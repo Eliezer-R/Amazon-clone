af53076d1005ce19a51b8a965578bdecd9fd90e0
Eliezer .R <ronaldalmanzarvasques@gmail.com>
2025-09-28 03:54:36 -0400

    Update AuthContext.jsx

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -33,7 +33,7 @@ export function AuthProvider ({ children }) {
 
   const fetchUser = async () => {
     try {
-      const response = await fetch(`${import.meta.env.VITE_API_URL}api/auth/me`, {
+      const response = await fetch(`${import.meta.env.VITE_API_URL}/api/auth/me`, {
         credentials: 'include'
       })
 
