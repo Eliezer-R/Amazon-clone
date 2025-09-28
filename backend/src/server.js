6d5dc2540a9baa626a6e1f4ed47128ae273460ed
Eliezer .R <ronaldalmanzarvasques@gmail.com>
2025-09-28 03:53:37 -0400

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -13,7 +13,7 @@ const app = express()
 const PORT = process.env.PORT || 5000
 const allowedOrigins = [
   "http://localhost:3000",
-  "https://amazon-clone-one-sandy.vercel.app/"
+  "https://amazon-clone-one-sandy.vercel.app"
 ];
 
 app.use(cors({
